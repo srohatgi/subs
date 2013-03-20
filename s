392a9e32a9fb169017649d9c4fd707f71ec18bbf
Sumeet Rohatgi <sumeet_rohatgi@hotmail.com>
2013-03-20 10:23:00 -0700

    modularized

--- s
+++ s
@@ -3,18 +3,11 @@
 
 function control_c
 {
-  pid=`ps aux | grep mongod | grep -v grep | awk '{ print $2 }'`
-  if [[ "$pid" == "" ]] 
-  then 
-    echo "unable to locate mongod pid" 
-  else
-    echo "stopping mongod with $pid"
-    kill $pid
-  fi
+  scripts/mongod_init.sh stop
 }
 
 trap control_c SIGINT
 make
 rm -f data/mongod.log.*
-scripts/mongod_init.sh
+scripts/mongod_init.sh start
 nodemon -e '.coffee|.jade|.json|.html|.css|.hbs' server/lib/dev.js
--- scripts/mongod_init.sh
+++ scripts/mongod_init.sh
@@ -1,3 +1,56 @@
 #!/bin/bash
 
-mongod --dbpath data --logpath data/mongod.log --fork
+rc=0
+myname=`basename $0`
+mongod_pid()
+{
+  pid=`ps aux | grep mongod | grep -v grep | grep -v $myname | awk '{ print $2 }'`
+  echo $pid
+}
+
+status()
+{
+  pid=$(mongod_pid)
+  if [[ "$pid" == "" ]]
+  then
+    echo "mongod is not running!"
+  else
+    echo "mongod is running with $pid"
+  fi
+}
+
+stop()
+{
+  pid=$(mongod_pid)
+  if [[ "$pid" == "" ]]
+  then
+    echo "unable to locate mongod pid"
+    rc=1
+  else
+    echo "stopping mongod with $pid"
+    kill $pid
+  fi
+}
+
+start()
+{
+  mongod --dbpath data --logpath data/mongod.log --fork
+  rc=$?
+}
+
+case $1 in
+start)
+  start
+;; 
+stop)   
+  stop
+;; 
+restart)
+  stop
+  start
+;;
+status)
+  status
+;; 
+esac    
+exit $rc
